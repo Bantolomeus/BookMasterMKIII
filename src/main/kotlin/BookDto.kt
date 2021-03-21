023485b625e6e690ccbe80ff490ba249021c2ea3
Christian Bantel <christian.bantel@dbfuhrpark.com>
2021-03-21 19:50:33 +0100

    Initial commit

---
+++ src/main/kotlin/BookDto.kt
@@ -0,0 +1,13 @@
+external interface Book {
+    val title: String
+    val author: String
+    val pages: Long
+    val coverUrl: String
+}
+
+data class DBook(
+    override val title: String,
+    override val author: String,
+    override val pages: Long,
+    override val coverUrl: String
+): Book
