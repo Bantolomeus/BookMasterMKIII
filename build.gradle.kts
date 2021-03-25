87b626d8a7b7579ccf5faf22a6ecd1506302afb4
Christian Bantel <christian.bantel@dbfuhrpark.com>
2021-03-25 02:52:14 +0100

    Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
 }
 
 dependencies {
+    
     implementation("org.jetbrains:kotlin-react:17.0.1-pre.148-kotlin-1.4.21")
     implementation("org.jetbrains:kotlin-react-dom:17.0.1-pre.148-kotlin-1.4.21")
     implementation(npm("react", "17.0.1"))
