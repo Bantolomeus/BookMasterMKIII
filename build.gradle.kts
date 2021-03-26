b464744c953ec1dba6fc60063705ef77f27f859f
Christian Bantel <christian.bantel@dbfuhrpark.com>
2021-03-26 17:37:37 +0100

    #8 Transform kotlin/js project into kotlin multiplatform one

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,17 @@
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
+
+val kotlinVersion = "1.4.0"
+val serializationVersion = "1.0.0-RC"
+val ktorVersion = "1.4.0"
+
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.4.31"
+    kotlin("multiplatform") version "1.4.0"
+    application //to run JVM part
+    kotlin("plugin.serialization") version "1.4.0"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.bantolomeus"
+version = "0.1"
 
 repositories {
     maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
@@ -12,26 +20,122 @@ repositories {
     jcenter()
 }
 
+//kotlin {
+//    js {
+//        browser {
+//            commonWebpackConfig {
+//                cssSupport.enabled = true
+//            }
+//        }
+//        binaries.executable()
+//    }
+//}
+
 kotlin {
+    jvm {
+        withJava()
+    }
     js {
         browser {
-            commonWebpackConfig {
-                cssSupport.enabled = true
+            binaries.executable()
+        }
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
             }
         }
-        binaries.executable()
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-serialization:$ktorVersion")
+                implementation("io.ktor:ktor-server-core:$ktorVersion")
+                implementation("io.ktor:ktor-server-netty:$ktorVersion")
+                implementation("ch.qos.logback:logback-classic:1.2.3")
+                implementation("io.ktor:ktor-websockets:$ktorVersion")
+                implementation("org.litote.kmongo:kmongo-coroutine-serialization:4.1.1")
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-js:$ktorVersion") //include http&websockets
+
+                //ktor client js json
+                implementation("io.ktor:ktor-client-json-js:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization-js:$ktorVersion")
+
+                implementation("org.jetbrains:kotlin-react:16.13.1-pre.110-kotlin-1.4.0")
+                implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.110-kotlin-1.4.0")
+                implementation(npm("react", "16.13.1"))
+                implementation(npm("react-dom", "16.13.1"))
+            }
+        }
     }
 }
 
-dependencies {
-    
-    implementation("org.jetbrains:kotlin-react:17.0.1-pre.148-kotlin-1.4.21")
-    implementation("org.jetbrains:kotlin-react-dom:17.0.1-pre.148-kotlin-1.4.21")
-    implementation(npm("react", "17.0.1"))
-    implementation(npm("react-dom", "17.0.1"))
+//dependencies {
+//
+//    implementation("org.jetbrains:kotlin-react:17.0.1-pre.148-kotlin-1.4.21")
+//    implementation("org.jetbrains:kotlin-react-dom:17.0.1-pre.148-kotlin-1.4.21")
+//    implementation(npm("react", "17.0.1"))
+//    implementation(npm("react-dom", "17.0.1"))
+//
+//    implementation("org.jetbrains:kotlin-styled:5.2.1-pre.148-kotlin-1.4.21")
+//    implementation(npm("styled-components", "~5.2.1"))
+//
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+//}
 
-    implementation("org.jetbrains:kotlin-styled:5.2.1-pre.148-kotlin-1.4.21")
-    implementation(npm("styled-components", "~5.2.1"))
+application {
+    mainClassName = "ServerKt"
+}
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+// include JS artifacts in any JAR we generate
+tasks.getByName<Jar>("jvmJar") {
+    val taskName = if (project.hasProperty("isProduction")) {
+        "jsBrowserProductionWebpack"
+    } else {
+        "jsBrowserDevelopmentWebpack"
+    }
+    val webpackTask = tasks.getByName<KotlinWebpack>(taskName)
+    dependsOn(webpackTask) // make sure JS gets compiled first
+    from(File(webpackTask.destinationDirectory, webpackTask.outputFileName)) // bring output file along into the JAR
+}
+
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+}
+
+distributions {
+    main {
+        contents {
+            from("$buildDir/libs") {
+                rename("${rootProject.name}-jvm", rootProject.name)
+                into("lib")
+            }
+        }
+    }
+}
+
+// Alias "installDist" as "stage" (for cloud providers)
+tasks.create("stage") {
+    dependsOn(tasks.getByName("installDist"))
+}
+
+tasks.getByName<JavaExec>("run") {
+    classpath(tasks.getByName<Jar>("jvmJar")) // so that the JS artifacts generated by `jvmJar` can be found and served
 }
