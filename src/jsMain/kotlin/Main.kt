7b665069b2b8844c31e08c79fb08f0543f27f7fa
Christian Bantel <christian.bantel@dbfuhrpark.com>
2021-03-27 18:20:18 +0100

    #8 Add simple api server

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,10 +1,11 @@
 import kotlinx.browser.document
+import react.child
 import react.dom.render
 
 
 @ExperimentalJsExport
 fun main() {
     render(document.getElementById("root")) {
-        child(App::class) {}
+        child(App)
     }
 }
--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,16 +1,61 @@
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    val port = System.getenv("PORT")?.toInt() ?: 8080
+    val port = System.getenv("PORT")?.toInt() ?: 8081
+
+    val shoppingList = mutableListOf(
+        ShoppingListItem("Cucumbers \uD83E\uDD52", 1),
+        ShoppingListItem("Tomatoes \uD83C\uDF45", 2),
+        ShoppingListItem("Orange Juice \uD83C\uDF4A", 3)
+    )
+
     embeddedServer(Netty, port) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(CORS) {
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Delete)
+            anyHost()
+        }
+        install(Compression) {
+            gzip()
+        }
         routing {
-            get("/") {
-                call.respondText("Hello there")
+            route(ShoppingListItem.path) {
+                get {
+                    call.respond(shoppingList)
+                }
+                post {
+                    shoppingList += call.receive<ShoppingListItem>()
+                    call.respond(HttpStatusCode.OK)
+                }
+                delete("/{id}") {
+                    val id = call.parameters["id"]?.toInt() ?: error("Invalid delete request")
+                    shoppingList.removeIf { it.id == id}
+                    call.respond(HttpStatusCode.OK)
+                }
             }
+//            get("/") {
+//                call.respondText(
+//                    this::class.java.classLoader.getResource("index.html")!!.readText(),
+//                    ContentType.Text.Html
+//                )
+//                call.respondText("Hello there")
+//            }
+//            static("/") {
+//                resource("")
+//            }
         }
     }.start(wait = true)
 }
